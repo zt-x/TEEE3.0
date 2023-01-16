006de49eb92b829b681c5e288a56ae5ee8050d43
许正韬 <1753059613@qq.com>
2023-01-16 12:56:56 +0800

    Init Git

---
+++ src/main/java/com/teee/SpringbootTeeeRebuildApplication.java
@@ -0,0 +1,13 @@
+package com.teee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTeeeRebuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTeeeRebuildApplication.class, args);
+    }
+
+}
