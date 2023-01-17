042f447b95b4bffd9d49c4944616b9495b669b4b
许正韬 <1753059613@qq.com>
2023-01-17 19:38:31 +0800

    java

---
+++ src/main/java/com/teee/Run.java
@@ -0,0 +1,17 @@
+package com.teee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xu ZhengTao
+ * @version 3.0
+ */
+@SpringBootApplication
+public class Run {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+
+}
